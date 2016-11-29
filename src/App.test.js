35f767839cc0055abde578f6a7026a76d5b7ba95
German Escobar <german.escobarc@gmail.com>
2016-11-29 02:16:54 -0500

    Implement challenge

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('renders the text field', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('input[type="text"]').length).toBe(1);
+});
+
+it('renders the paragraph', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('p.repeater').length).toBe(1);
+});
+
+it('repeats the text', () => {
+  const wrapper = shallow(<App />);
+  wrapper.find('input[type="text"]').simulate('change', { target: { value: 'My new value' } });
+  expect(wrapper.find('p.repeater').text()).toBe("My new value");
 });
